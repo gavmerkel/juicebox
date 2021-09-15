74c209d2b7d0066def09319efc82e12bb57df0d2
Jacob Merkel <soccerjacobjm17@gmail.com>
2021-09-14 20:02:04 -0400

    day 3 finished

---
+++ api/tags.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const tagsRouter = express.Router();
+const { getAllTags, getPostsByTagName } = require('../db');
+
+tagsRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+
+  next(); 
+});
+
+tagsRouter.get('/', async (req, res) => {
+  const tags = await getAllTags();
+
+  res.send({
+    tags
+  });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  const tagName = req.params;
+  try {
+    const allTags = await getPostsByTagName(tagName)
+
+    const tags = allTags.filter(post => {
+      return post.active || (req.user && post.author.id === req.user.id);
+    });
+    res.send({ posts: tags })
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = tagsRouter;
